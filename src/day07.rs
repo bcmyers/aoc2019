1e0818b1722730762edcfced4b1fac96b703c6c0
Brian Myers <brian.carl.myers@gmail.com>
2019-12-10 15:59:15 -0500

    day 8

--- src/day07.rs
+++ src/day07.rs
@@ -1,8 +1,7 @@
 use std::io;
-use std::sync::{Arc, Barrier};
-use std::thread;
+use std::sync::Barrier;
 
-use crossbeam::channel;
+use crossbeam::{channel, thread};
 use itertools::Itertools;
 
 use crate::computer::{Channel, Computer, Rom};
@@ -29,92 +28,98 @@ pub fn run<R>(reader: R) -> Result<(String, String), Error>
 where
     R: io::BufRead,
 {
-    let rom = Rom::from_reader(reader)?;
     let ncomputers = 5;
 
-    let (tx_output, rx_output) = channel::bounded(fact(ncomputers)?);
+    let barrier = Barrier::new(ncomputers);
+    let rom = Rom::from_reader(reader)?;
 
-    let mut handles = Vec::new();
-    let mut senders = Vec::new();
-    let barrier = Arc::new(Barrier::new(ncomputers));
-    for i in 0..ncomputers {
-        let (tx_input, rx_input) = channel::bounded(fact(ncomputers)?);
-        senders.push(tx_input);
+    let (answer1, answer2) = thread::scope(|s| {
+        let (tx_output, rx_output) = channel::bounded(fact(ncomputers)?);
 
-        let barrier = barrier.clone();
-        let rom = rom.clone();
-        let tx_output = tx_output.clone();
+        let mut handles = Vec::new();
+        let mut senders = Vec::new();
+        for i in 0..ncomputers {
+            let (tx_input, rx_input) = channel::bounded(fact(ncomputers)?);
+            senders.push(tx_input);
 
-        let handle = thread::spawn(move || {
-            loop {
-                let (part, phase_setting, input, output) = match rx_input.recv() {
-                    Ok(data) => data,
-                    Err(_) => break,
-                };
-                let mut computer = Computer::new(input, output);
+            let barrier = &barrier;
+            let rom = &rom;
+            let tx_output = tx_output.clone();
 
-                computer.input_mut().push_back(phase_setting);
-                if i == 0 {
-                    computer.input_mut().push_back(0);
+            let handle = s.spawn(move |_| {
+                loop {
+                    let (part, phase_setting, input, output) = match rx_input.recv() {
+                        Ok(data) => data,
+                        Err(_) => break,
+                    };
+                    let mut computer = Computer::new(input, output);
+
+                    computer.input_mut().push_back(phase_setting);
+                    if i == 0 {
+                        computer.input_mut().push_back(0);
+                    }
+
+                    barrier.wait();
+                    computer.execute(rom, None)?;
+                    barrier.wait();
+
+                    if i == 4 {
+                        let answer = computer.output_mut().pop_front()?;
+                        tx_output.send((part, answer)).unwrap();
+                    }
                 }
+                Ok::<_, Error>(())
+            });
+            handles.push(handle);
+        }
 
-                barrier.wait();
-                computer.execute(&rom, None)?;
-                barrier.wait();
-
-                if i == 4 {
-                    let answer = computer.output_mut().pop_front()?;
-                    tx_output.send((part, answer)).unwrap();
+        for (part, range) in (&[(0..5), (5..10)]).iter().cloned().enumerate() {
+            for phase_settings in range.map(|i| i as i64).permutations(ncomputers) {
+                let channels = (0..ncomputers)
+                    .map(|_| Channel::default())
+                    .collect::<Vec<_>>();
+                let mut outputs = (0..ncomputers).map(|i| channels[i].clone());
+                let mut inputs =
+                    (0..ncomputers).map(|i| channels[(i + ncomputers - 1) % ncomputers].clone());
+                for i in 0..ncomputers {
+                    let output = outputs.next().unwrap();
+                    let input = inputs.next().unwrap();
+                    senders[i]
+                        .send((part, phase_settings[i], input, output))
+                        .unwrap();
                 }
-            }
-            Ok::<_, Error>(())
-        });
-        handles.push(handle);
-    }
-
-    for (part, range) in (&[(0..5), (5..10)]).iter().cloned().enumerate() {
-        for phase_settings in range.map(|i| i as i64).permutations(ncomputers) {
-            let channels = (0..ncomputers)
-                .map(|_| Channel::default())
-                .collect::<Vec<_>>();
-            let mut outputs = (0..ncomputers).map(|i| channels[i].clone());
-            let mut inputs =
-                (0..ncomputers).map(|i| channels[(i + ncomputers - 1) % ncomputers].clone());
-            for i in 0..ncomputers {
-                let output = outputs.next().unwrap();
-                let input = inputs.next().unwrap();
-                senders[i]
-                    .send((part, phase_settings[i], input, output))
-                    .unwrap();
             }
         }
-    }
 
-    drop(senders);
-    drop(tx_output);
+        drop(senders);
+        drop(tx_output);
 
-    let (mut answer1, mut answer2) = (0, 0);
+        let (mut answer1, mut answer2) = (0, 0);
 
-    let mut iter = rx_output.iter();
-    while let Some((part, output)) = iter.next() {
-        match part {
-            0 => {
-                if output > answer1 {
-                    answer1 = output;
+        let mut iter = rx_output.iter();
+        while let Some((part, output)) = iter.next() {
+            match part {
+                0 => {
+                    if output > answer1 {
+                        answer1 = output;
+                    }
                 }
-            }
-            1 => {
-                if output > answer2 {
-                    answer2 = output;
+                1 => {
+                    if output > answer2 {
+                        answer2 = output;
+                    }
                 }
+                _ => unreachable!(),
             }
-            _ => unreachable!(),
         }
-    }
 
-    for handle in handles {
-        handle.join().unwrap()?;
-    }
+        for handle in handles {
+            handle.join().unwrap()?;
+        }
+
+        Ok::<_, Error>((answer1, answer2))
+    })
+    .unwrap()?;
 
     Ok((answer1.to_string(), answer2.to_string()))
 }
--- src/day08.rs
+++ src/day08.rs
@@ -2,10 +2,62 @@ use std::io;
 
 use crate::error::Error;
 
-pub fn run<R>(reader: R) -> Result<(String, String), Error>
+const ROWS: usize = 6;
+const COLS: usize = 25;
+
+pub fn run<R>(mut reader: R) -> Result<(String, String), Error>
 where
     R: io::BufRead,
 {
-    let _ = reader;
-    Ok(("foo".to_string(), "bar".to_string()))
+    // Parse input
+    let mut buf = Vec::new();
+    reader.read_to_end(&mut buf)?;
+    buf.pop();
+    buf.iter_mut().for_each(|b| *b = *b - 48);
+
+    // Part 1
+    let answer1 = match buf
+        .chunks(ROWS * COLS)
+        .fold((std::usize::MAX, None), |mut state, layer| {
+            let nzeros = layer.iter().filter(|b| **b == 0).count();
+            if nzeros < state.0 {
+                state = (nzeros, Some(layer));
+            }
+            state
+        }) {
+        (_, Some(layer)) => {
+            let nones = layer.iter().filter(|b| **b == 1).count();
+            let ntwos = layer.iter().filter(|b| **b == 2).count();
+            nones * ntwos
+        }
+        (_, None) => bail!("TODO"),
+    };
+
+    // Part 2
+    let image = buf
+        .chunks(ROWS * COLS)
+        .fold([2u8; ROWS * COLS], |mut state, layer| {
+            state.iter_mut().enumerate().for_each(|(i, b)| {
+                if *b == 2 {
+                    *b = layer[i];
+                }
+            });
+            state
+        });
+
+    let mut iter = image.iter();
+    let mut answer2 = String::new();
+    for _row in 0..ROWS {
+        for _col in 0..COLS {
+            match iter.next() {
+                Some(0) => answer2.push('\u{2585}'),
+                Some(1) => answer2.push(' '),
+                Some(_) => bail!("TODO"),
+                None => bail!("TODO"),
+            }
+        }
+        answer2.push('\n');
+    }
+
+    Ok((answer1.to_string(), answer2.to_string()))
 }
